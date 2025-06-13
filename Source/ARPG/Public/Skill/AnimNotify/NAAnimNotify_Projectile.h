10e8f1efde167fbbd014fbc82280ee4530fec051
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-06-13 11:09:23 +0900

    notify

---
+++ Source/ARPG/Public/Skill/AnimNotify/NAAnimNotify_Projectile.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "NAAnimNotify_Projectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UNAAnimNotify_Projectile : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
